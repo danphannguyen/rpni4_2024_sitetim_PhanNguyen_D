22452a1711b766cf8f12ee62c0fd1fd8a6d4292f
Dan PN <77532827+Derlvx@users.noreply.github.com>
2024-03-11 14:02:41 -0400

    favicon

--- ressources/vues/gabarit.blade.php
+++ ressources/vues/gabarit.blade.php
@@ -4,6 +4,7 @@
         <title>@yield('title')</title>
         <link rel="stylesheet" href="../public/liaisons/css/styles.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <link rel="shortcut icon" href="../public/liaisons/svg/logo-black.svg" type="image/x-icon">
         <meta charset="utf-8">
     </head>
     <body class="light-theme">
